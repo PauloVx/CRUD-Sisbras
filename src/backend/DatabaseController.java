e96fa2dd7efc6b1260c10973a40663ad37dde9ce
Paulo <paulituvitu@hotmail.com>
2021-01-22 11:59:12 -0300

    Arquivo .properties para usuario e senha do banco

--- src/backend/DatabaseController.java
+++ src/backend/DatabaseController.java
@@ -1,27 +1,37 @@
 package backend;
 
+import java.io.IOException;
 import java.sql.*;
+import java.util.Properties;
 
 public class DatabaseController {
 	
 	public static Connection getConnection() {
-		final String connectionString = "jdbc:postgresql://localhost:5432/crudsisbras";
-		final String user = "postgres";
-		final String password = "admin";
-		
 		Connection connection;
 		
 		try {
+			Properties props = getProperties();
+			final String connectionString = props.getProperty("banco.url");
+			final String user = props.getProperty("banco.usuario");
+			final String password  = props.getProperty("banco.senha");
+			
 			connection = DriverManager.getConnection(connectionString, user, password);
 			System.out.println("Conectado ao PostgreSQL");
 			
 			return connection;
 		}
-		catch (SQLException e) {
+		catch (SQLException | IOException e) {
 			e.printStackTrace();
 			
 			return null;
 		}
+	}
+	
+	private static Properties getProperties() throws IOException {
+		Properties arquivoProperties = new Properties();
+		String caminho = "/conexao-banco.properties";
+		arquivoProperties.load(DatabaseController.class.getResourceAsStream(caminho));
+		return arquivoProperties;
 	}
 	
 }
